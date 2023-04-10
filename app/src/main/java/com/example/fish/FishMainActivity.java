61f0244ec35ab9247975db45dcbf47b299e9792e
judereynolds <judereynolds100@gmail.com>
2023-04-10 15:42:54 -0800

    First Commit?

---
+++ app/src/main/java/com/example/fish/FishMainActivity.java
@@ -0,0 +1,21 @@
+package com.example.fish;
+
+import com.example.GameFramework.GameMainActivity;
+import com.example.GameFramework.gameConfiguration.GameConfig;
+import com.example.GameFramework.infoMessage.GameState;
+import com.example.GameFramework.LocalGame;
+
+
+public class FishMainActivity extends GameMainActivity {
+
+    @Override
+        public GameConfig createDefaultConfig() {
+            return null;
+        }
+
+
+    @Override
+    public LocalGame createLocalGame(GameState gameState) {
+        return null;
+    }
+}
