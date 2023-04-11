c9bc1abd3f00d674ace9ea0680a41320a9918c45
judereynolds <judereynolds100@gmail.com>
2023-04-10 21:55:36 -0800

    FishDumbAi is finished

--- app/src/main/java/com/example/fish/FishAskAction.java
+++ app/src/main/java/com/example/fish/FishAskAction.java
@@ -1,4 +1,15 @@
 package com.example.fish;
 
-public class FishAskAction {
+import com.example.GameFramework.actionMessage.GameAction;
+import com.example.GameFramework.players.GamePlayer;
+
+public class FishAskAction extends GameAction {
+    /**
+     * constructor for GameAction
+     *
+     * @param player the player who created the action
+     */
+    public FishAskAction(GamePlayer player) {
+        super(player);
+    }
 }
--- app/src/main/java/com/example/fish/FishDumbAI.java
+++ app/src/main/java/com/example/fish/FishDumbAI.java
@@ -3,6 +3,8 @@ package com.example.fish;
 import com.example.GameFramework.players.GameComputerPlayer;
 import com.example.GameFramework.infoMessage.GameInfo;
 
+import java.util.Random;
+
 public class FishDumbAI extends GameComputerPlayer {
 
     public FishDumbAI(String name) {
@@ -11,7 +13,22 @@ public class FishDumbAI extends GameComputerPlayer {
 
     @Override
     protected void receiveInfo(GameInfo info) {
+        FishGameState recieve = new FishGameState((FishGameState)info);
 
+        try {
+            Thread.sleep(1000);
+            if(recieve.getCurrentPlayer() != this.playerNum) {
+                return;
+            }
+            else {
+                FishAskAction play = new FishAskAction(this);
+                game.sendAction(play);
+            }
+
+        }
+        catch (Exception e) {
+
+        }
     }
 
 
